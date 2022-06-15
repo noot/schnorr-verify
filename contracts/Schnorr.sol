8fb7519f92a1150f554c29f472503f1b186aef5c
noot <elizabethjbinks@gmail.com>
2022-06-15 17:26:29 -0400

    initial commit

---
+++ contracts/Schnorr.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: LGPLv3
+pragma solidity ^0.8.0;
+
+contract Schnorr {
+    // sr := -s*P_x
+    // er := -e*P_x
+    // px := public key x-coord
+    // parity := public key y-coord parity (27 or 28)
+    // message := 32-byte message
+    // e := schnorr signature challenge
+    function verify(bytes32 sr, bytes32 er, bytes32 px, uint8 parity, bytes32 message, bytes32 e) public pure returns (bool) {
+        // ecrecover = (m, v, r, s);
+        address q = ecrecover(sr, parity, px, er);
+        return e == keccak256(abi.encodePacked(q, message));
+    }
+}
